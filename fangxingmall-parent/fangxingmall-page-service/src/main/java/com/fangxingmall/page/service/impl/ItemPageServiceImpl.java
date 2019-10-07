076569d0970623bd0ff41d16c40d03f1f52f936b
fangchenyong <1013659102@qq.com>
2019-10-07 15:09:40 +0800

    整合FreeMarker

--- fangxingmall-parent/fangxingmall-page-service/src/main/java/com/fangxingmall/page/service/impl/ItemPageServiceImpl.java
+++ fangxingmall-parent/fangxingmall-page-service/src/main/java/com/fangxingmall/page/service/impl/ItemPageServiceImpl.java
@@ -3,6 +3,7 @@ package com.fangxingmall.page.service.impl;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,14 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.fangxingmall.mapper.TbGoodsDescMapper;
 import com.fangxingmall.mapper.TbGoodsMapper;
+import com.fangxingmall.mapper.TbItemCatMapper;
+import com.fangxingmall.mapper.TbItemMapper;
 import com.fangxingmall.page.service.ItemPageService;
 import com.fangxingmall.pojo.TbGoods;
 import com.fangxingmall.pojo.TbGoodsDesc;
+import com.fangxingmall.pojo.TbItem;
+import com.fangxingmall.pojo.TbItemExample;
+import com.fangxingmall.pojo.TbItemExample.Criteria;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -32,7 +38,13 @@ public class ItemPageServiceImpl implements ItemPageService{
 	
 	@Autowired
 	private TbGoodsDescMapper goodsDescMapper;
-		
+	
+	@Autowired
+	private TbItemCatMapper tbItemCatMapper;
+	
+	@Autowired
+	private TbItemMapper itemMapper;
+	
 	@Override
 	public boolean genItemHtml(Long goodsId){				
 		try {
@@ -45,7 +57,23 @@ public class ItemPageServiceImpl implements ItemPageService{
 			dataModel.put("goods", goods);			
 			//2.加载商品扩展表数据			
 			TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
-			dataModel.put("goodsDesc", goodsDesc);						
+			dataModel.put("goodsDesc", goodsDesc);		
+			//3.读取商品分类
+			String itemCat1 = tbItemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+			String itemCat2 = tbItemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+			String itemCat3 = tbItemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+			dataModel.put("itemCat1", itemCat1);
+			dataModel.put("itemCat2", itemCat2);
+			dataModel.put("itemCat3", itemCat3);
+			//4.SKU列表			
+			TbItemExample example=new TbItemExample();
+			Criteria criteria = example.createCriteria();
+			criteria.andStatusEqualTo("1");//状态为有效
+			criteria.andGoodsIdEqualTo(goodsId);//指定SPU ID
+			example.setOrderByClause("is_default desc");//按照状态降序，保证第一个为默认			
+			List<TbItem> itemList = itemMapper.selectByExample(example);		
+			dataModel.put("itemList", itemList);
+
 			Writer out=new FileWriter(pagedir+goodsId+".html");
 			template.process(dataModel, out);
 			out.close();
